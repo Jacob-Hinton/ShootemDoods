5b735a323448090bc01135a1bfd4c8bc16eb9c17
Jacob Hinton <jacobmhinton@gmail.com>
2016-11-20 22:03:53 -0600

    added turrets

---
+++ Assets/Scripts/enemy_scripts/Point_at_player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/****************************************************************************************/
+/*
+/* FILE NAME: Point_At_Player
+/*
+/* DESCRIPTION: this script controls the behavior of an object which must change it's angle to point at the player
+/*
+/*
+/* DATE     BY     			  DESCRIPTION
+/* ======== ======= 		  =============
+/* 11/20/16 Brandon           created script + header
+/*
+/****************************************************************************************/
+
+public class Point_At_Player : MonoBehaviour {
+	private float target;
+	public float speed = 50.0F;
+
+	// Use this for initialization
+	void Start () {
+		target = Vector3.Angle (GameObject.FindWithTag("Player").transform.position, this.gameObject.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float angle = Mathf.MoveTowardsAngle (transform.eulerAngles.y, target, speed * Time.deltaTime);
+		transform.eulerAngles = new Vector3 (angle, 0, 0);
+	}
+}
---
+++ Assets/Scripts/enemy_scripts/Shoot_Bullet.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//****************************************************************************************/
+/*
+/* FILE NAME: Shoot_Bullet
+/*
+/* DESCRIPTION: this script controls the behavior of an object which shoots bullets
+/*
+/*
+/* DATE     BY     			  DESCRIPTION
+/* ======== ======= 		  =============
+/* 11/20/16 Brandon           created script + header
+/*
+/****************************************************************************************/
+
+public class Shoot_Bullet : MonoBehaviour {
+
+	private bool canShoot = false;
+	private bool coRoutineIsStarted = false;
+	public float pauseTime = 1.5f;
+	public Rigidbody2D projectile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (canShoot) {
+			Rigidbody2D clone;
+			clone = Instantiate (projectile, transform.position + new Vector3 (0, 0, 0), transform.rotation) as Rigidbody2D;
+			canShoot = false;
+		} else if (!coRoutineIsStarted) {
+			StartCoroutine (bulletWait ());
+		}
+	}
+
+	public IEnumerator bulletWait() {
+		coRoutineIsStarted = true;
+		canShoot = false;
+		yield return new WaitForSeconds (pauseTime);
+		canShoot = true;
+		coRoutineIsStarted = false;
+	}
+}
