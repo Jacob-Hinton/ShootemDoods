4d0c2f90cabc67dce0746d06da8d8fa1c2d9097d
blkilpatrick42@gmail.com <blkilpatrick42@gmail.com>
2016-11-23 13:36:16 -0600

    Added main menu

---
+++ Assets/Scripts/Main_Menu_Scripts/moon_movement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/****************************************************************************************/
+/*
+/* FILE NAME: moon_movement
+/*
+/* DESCRIPTION: this script controls the slow rising of the moon on the title screen/main menu
+/*
+/*
+/* DATE     BY     	DESCRIPTION
+/* ======== ======= =============
+/* 10/25/16	Jacob	created headr
+/*
+/*
+/****************************************************************************************/
+
+public class moon_movement : MonoBehaviour {
+
+    public float speed;
+    public Vector3 pos;
+    // Use this for initialization
+    void Start () {
+        pos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(4,2,0) * speed * Time.deltaTime;
+    }
+}
